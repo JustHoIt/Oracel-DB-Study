bd7f6e77ed0cdc1b8feba8c8a3d0665d9faa18eb
Just_Ho_it <zalop3128@kakao.com>
2024-01-15 07:47:42 +0900

    연습문제 - SQL문제 2

--- Oracle/PRACTICE/Chapter3pr.sql
+++ Oracle/PRACTICE/Chapter3pr.sql
@@ -27,7 +27,7 @@ SELECT SUM(SALEPRICE)
 FROM ORDERS
 WHERE CUSTID = 1;
 
---박지성이 구매한 도서의 수
+-- 박지성이 구매한 도서의 수
 SELECT COUNT(ORDERID) AS COUNTBOOK
 FROM ORDERS O
          INNER JOIN CUSTOMER C ON O.CUSTID = C.CUSTID
@@ -38,7 +38,7 @@ FROM ORDERS
 WHERE CUSTID = 1;
 
 
---박지성이 구매한 도서의 출판사 수
+-- 박지성이 구매한 도서의 출판사 수
 SELECT COUNT(PUBLISHER) AS COUNTPUBLISHER
 FROM BOOK B
          INNER JOIN ORDERS O ON B.BOOKID = O.BOOKID
@@ -53,7 +53,7 @@ FROM BOOK,
 WHERE BOOK.BOOKID = ORDERS.BOOKID
   AND ORDERS.CUSTID = 1;
 
---박지성이 구매한 도서의 이름, 가격, 정가와 판매가격의 차이
+-- 박지성이 구매한 도서의 이름, 가격, 정가와 판매가격의 차이
 SELECT B.BOOKNAME, B.PRICE, B.PRICE - O.SALEPRICE AS DIFF
 FROM BOOK B,
      CUSTOMER C,
@@ -63,7 +63,7 @@ WHERE B.BOOKID = O.BOOKID
   AND C.NAME = '박지성';
 
 
---박지성이 구매하지 않은 도서의 이름
+-- 박지성이 구매하지 않은 도서의 이름
 -- DISTINCT : 중복 데이터 제거
 SELECT DISTINCT B.BOOKNAME
 FROM BOOK B,
@@ -72,3 +72,101 @@ FROM BOOK B,
 WHERE B.BOOKID = O.BOOKID
   AND O.CUSTID = C.CUSTID
   AND C.NAME != '박지성';
+
+-- 마다서점의 도서의 총수
+SELECT COUNT(*)
+FROM BOOK;
+
+-- 마당서점에 도서를 출고하는 출판사의 총수
+SELECT COUNT(DISTINCT PUBLISHER)
+FROM BOOK;
+
+-- 모든 고객의 이름, 주소
+SELECT NAME, ADDRESS
+FROM CUSTOMER;
+
+-- 특정기간 사이에 주문받은 도서의 주문번호
+SELECT ORDERID
+FROM ORDERS
+WHERE ORDERDATE BETWEEN '20.07.04' AND '20.7.7';
+
+-- 특정기간을 제외한 주문받은 도서의 주문번호
+SELECT ORDERID
+FROM ORDERS
+MINUS
+SELECT ORDERID
+FROM ORDERS
+WHERE ORDERDATE BETWEEN '20.07.04' AND '20.7.7';
+
+SELECT ORDERID
+FROM ORDERS
+WHERE ORDERDATE NOT BETWEEN '20.07.04' AND '20.7.7';
+
+-- 성이 김씨인 고객의 이름과 주소
+SELECT NAME, ADDRESS
+FROM CUSTOMER
+WHERE NAME LIKE '김%';
+
+-- 성이 김씨이고 이름이 '아'로 끝나는 고객의 이름과 주소
+SELECT NAME, ADDRESS
+FROM CUSTOMER
+WHERE NAME LIKE '김%아';
+
+-- 주문하지 않은 고객의 이름(부속질의 사용)
+SELECT NAME
+FROM CUSTOMER
+WHERE CUSTID NOT IN (SELECT CUSTID
+                     FROM ORDERS);
+
+-- 주문금액의 총액과 주문의 평균 금액
+SELECT SUM(SALEPRICE) AS 총액, AVG(SALEPRICE) AS 평균액
+FROM ORDERS;
+
+--고객의 이름과 고객별 구매액
+SELECT C.NAME, SUM(O.SALEPRICE)
+FROM CUSTOMER C,
+     ORDERS O
+WHERE C.CUSTID = O.CUSTID
+GROUP BY C.NAME;
+
+-- 고객의 이름과 구매한 도서 목록
+SELECT C.NAME, B.BOOKNAME
+FROM CUSTOMER C,
+     ORDERS O,
+     BOOK B
+WHERE C.CUSTID = O.CUSTID
+  AND O.BOOKID = B.BOOKID;
+
+-- 도서의 가격(BOOK 테이블)과 판매가격(ORDERS 테이블)의 차이가 가장 많은 주문
+SELECT O.*
+FROM BOOK B,
+     ORDERS O
+WHERE B.BOOKID = O.BOOKID
+  AND B.PRICE - O.SALEPRICE = (SELECT MAX(B.PRICE - O.SALEPRICE)
+                               FROM BOOK B,
+                                    ORDERS O
+                               WHERE B.BOOKID = O.BOOKID);
+
+--도서의 판매액 편균보다 자신의 구매액 평균이 더 높은 고객의 이름
+SELECT C.NAME
+FROM ORDERS O,
+     CUSTOMER C
+WHERE O.CUSTID = C.CUSTID
+GROUP BY NAME
+HAVING AVG(SALEPRICE) > (SELECT AVG(SALEPRICE)
+                         FROM ORDERS);
+
+-- 박지성이 구매한 도서의 출판사와 같은 출판사에서 도서를 구매한 고객의 이름
+SELECT C.NAME
+FROM ORDERS O,
+     CUSTOMER C,
+     BOOK B
+WHERE O.CUSTID = C.CUSTID AND O.BOOKID = B.BOOKID
+  AND NAME NOT LIKE '박지성'
+  AND B.PUBLISHER IN (SELECT B.PUBLISHER
+                      FROM CUSTOMER C,
+                           BOOK B,
+                           ORDERS O
+                      WHERE C.CUSTID = O.CUSTID
+                        AND B.BOOKID = O.BOOKID
+                        AND NAME LIKE '박지성');
